7290c364314c767e70dac834e78f9afb9f4218df
Pytoritas <phmoon08@gmail.com>
2026-03-13 21:39:11 +0900

    feat: sync root gosm control example figure workflow

---
+++ src/stomatal_optimiaztion/domains/gosm/examples/__init__.py
@@ -0,0 +1,21 @@
+from stomatal_optimiaztion.domains.gosm.examples.control_figure import (
+    DEFAULT_CONTROL_FIGURE_SPEC_PATH,
+    DEFAULT_CONTROL_FIGURE_OUTPUT_DIR,
+    GOSMControlExampleFigureArtifacts,
+    GOSMControlExamplePayload,
+    build_control_example_payload,
+    compare_control_example_to_legacy_mat,
+    legacy_control_digest_summary,
+    render_control_example_figure_bundle,
+)
+
+__all__ = [
+    "DEFAULT_CONTROL_FIGURE_OUTPUT_DIR",
+    "DEFAULT_CONTROL_FIGURE_SPEC_PATH",
+    "GOSMControlExampleFigureArtifacts",
+    "GOSMControlExamplePayload",
+    "build_control_example_payload",
+    "compare_control_example_to_legacy_mat",
+    "legacy_control_digest_summary",
+    "render_control_example_figure_bundle",
+]
